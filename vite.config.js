211d40316d89f9da0632ec7923e19cab70ce4fe0
aakash <aakashpr975@gmail.com>
2025-03-29 17:59:01 +0530

    vercel.json fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+  },
+});
